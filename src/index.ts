70ce6d146824a7f5fe235ceade2040f1e736a3b1
Joshua Gore <Joshua_Gore@eagle.co.nz>
2021-05-28 16:27:58 +1200

    added tsconfig

--- src/index.ts
+++ src/index.ts
@@ -35,4 +35,4 @@ export {default as Map} from './Map';
 export {default as MapSceneView} from './MapSceneView';
 export {default as MapView} from './MapView';
 export {default as Search} from './Search';
-export {default as Basemap} from './Basemap';
+export {default as Basemap} from './BaseMap';
